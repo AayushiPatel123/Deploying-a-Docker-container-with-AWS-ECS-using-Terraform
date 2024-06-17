666c7de9ffc4f303a7ee9e5ed30832fe001cf9a8
AayushiPatel123 <patel9i5@uwindsor.ca>
2024-06-17 16:34:59 -0400

    Added terraform files

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Define required providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+# Configure the AWS provider
+provider "aws" {
+  region     = "us-east-1"  # Specify your desired AWS region
+}
+
+# Create an ECR repository
+resource "aws_ecr_repository" "app_ecr_repo" {
+  name = "app-repo"
+}
